ea358a702a21c154fde32b6743030ca1fb098bb4
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2023-11-12 11:04:01 +0100

    1.0.0

--- plex-playlist-sort.py
+++ plex-playlist-sort.py
@@ -1,106 +1,91 @@
 import json
 import sys
-import urllib.parse
 import urllib.request
 from dataclasses import dataclass
-from typing import Optional
+from typing import Any, Optional
 
 
 @dataclass
 class Config:
     baseurl: str
-    name: str
+    title: str
     headers: dict[str, str]
 
 
-def clear_playlist(config: Config, idx: str) -> dict:
+def get_playlist(config: Config, idx: str) -> Any:
     req = urllib.request.Request(
         f"{config.baseurl}/playlists/{idx}/items",
         headers=config.headers,
-        method="DELETE",
     )
     with urllib.request.urlopen(req) as response:
         return json.loads(response.read())
 
 
-def get_playlists(config: Config) -> dict:
+def get_playlist_id(config: Config) -> Optional[str]:
+    playlists = get_playlists(config)
+    for container in playlists.values():
+        for playlist in container["Metadata"]:
+            if playlist["title"] == config.title and isinstance(playlist["ratingKey"], str):
+                return playlist["ratingKey"]
+    return None
+
+
+def get_playlists(config: Config) -> Any:
     req = urllib.request.Request(f"{config.baseurl}/playlists/all", headers=config.headers)
     with urllib.request.urlopen(req) as response:
         return json.loads(response.read())
 
 
-def get_playlist(config: Config, idx: str) -> dict:
-    req = urllib.request.Request(
-        f"{config.baseurl}/playlists/{idx}/items",
-        headers=config.headers,
-    )
-    with urllib.request.urlopen(req) as response:
-        return json.loads(response.read())
-
-
-def get_server_id(config: Config) -> dict:
-    req = urllib.request.Request(f"{config.baseurl}/identity", headers=config.headers)
-    with urllib.request.urlopen(req) as response:
-        return json.loads(response.read())
-
-
-def add_to_playlist(config: Config, idx: str, server_id: str, playqueue_ids: list[int]):
-    query_params = f"uri=server://{server_id}/com.plexapp.plugins.library/library/metadata/{','.join(playqueue_ids)}"
-    req = urllib.request.Request(
-        f"{config.baseurl}/playlists/{idx}/items?{query_params}",
-        headers=config.headers,
-        method="PUT",
-    )
-    with urllib.request.urlopen(req) as response:
-        return json.loads(response.read())
-
-
-def get_playlist_id(config: Config, name: str) -> Optional[str]:
-    playlists = get_playlists(config)
-    for container in playlists.values():
-        for playlist in container["Metadata"]:
-            if playlist["title"] == name:
-                return playlist["ratingKey"]
-    return None
-
-
-def sort_playlist(playlist: dict):
+def sort_playlist(playlist: Any) -> None:
     playlist["MediaContainer"]["Metadata"].sort(key=lambda x: x["title"])
 
 
-def main():
+def update_playlist_order(config: Config, idx: str, items: list[int]) -> None:
+    query = ""
+    for i, item in enumerate(items):
+        if i > 0:
+            query = f"?after={items[i - 1]}"
+
+        req = urllib.request.Request(
+            f"{config.baseurl}/playlists/{idx}/items/{item}/move{query}",
+            headers=config.headers,
+            method="PUT",
+        )
+        urllib.request.urlopen(req)
+        print(f"Sorted {i + 1} of {len(items)} items...", end="\r")
+    print()
+
+
+def main() -> None:
+    if len(sys.argv) != 4:
+        print(f"Usage: {sys.argv[0]} <Plex URL> <Access Token> <Playlist Title>")
+        exit(1)
+
     config = Config(
         baseurl=sys.argv[1],
-        name=sys.argv[3],
+        title=sys.argv[3],
         headers={
             "Accept": "application/json",
-            "Content-Type": "application/json",
             "X-Plex-Token": sys.argv[2],
         },
     )
-    server_id = get_server_id(config)
 
-    playlist_id = get_playlist_id(config, config.name)
+    playlist_id = get_playlist_id(config)
     if not playlist_id:
-        print(f"Playlist not found: {config.name}")
+        print(f"Playlist not found: '{config.title}'")
         exit(1)
+    print(f"Found Playlist '{config.title}'")
 
     playlist = get_playlist(config, playlist_id)
     if playlist["MediaContainer"]["smart"]:
-        print(f"Can only sort dumb playlist, this one is smart: {config.name}")
+        print(f"Can't only sort smart playlist: '{config.title}'")
         exit(1)
 
     sort_playlist(playlist)
-    playqueue_ids = [item["ratingKey"] for item in playlist["MediaContainer"]["Metadata"]]
+    playlist_items = [item["playlistItemID"] for item in playlist["MediaContainer"]["Metadata"]]
 
-    clear_playlist(config, playlist_id)
-    result = add_to_playlist(
-        config,
-        playlist_id,
-        server_id["MediaContainer"]["machineIdentifier"],
-        playqueue_ids,
-    )
-    print(result)
+    update_playlist_order(config, playlist_id, playlist_items)
+    print("Done")
 
 
 if __name__ == "__main__":
